16c73fa8248dd4b2e3fa14a54046456e68e408f7
mian <38103855+intmian@users.noreply.github.com>
2024-02-17 21:32:52 +0800

    优化了监控显示

--- frontend/src/admin/servicesData.jsx
+++ frontend/src/admin/servicesData.jsx
@@ -1,6 +1,9 @@
-import {Button, List, Space, Spin} from "antd";
+import {Button, Card, Flex, List, Space, Spin, Tooltip} from "antd";
+import {TimeFromStart} from "../common/misc.jsx";
 
-function service({name, startTime, status}) {
+const {Meta} = Card;
+
+function ServiceInfo({name, startTime, status}) {
     let open = false;
     if (status === 'start') {
         open = true;
@@ -19,16 +22,28 @@ function service({name, startTime, status}) {
             name = '自动任务平台';
             break;
         default:
+
     }
-
     let buttonStr = open ? '关闭' : '开启';
-
-    return <Space>
-        {name}
-        {startTime}
-        {status}
-        <Button type="primary">{buttonStr}</Button>
-    </Space>
+    return <Card
+        style={{
+            width: 330,
+        }}
+    >
+        <Meta title={name}/>
+        <Space>
+            <Tooltip title={"状态"}>
+                <div>{status}</div>
+            </Tooltip>
+            <Tooltip title={"启动时间"}>
+                <TimeFromStart
+                    startTime={startTime}
+                    width={150}
+                />
+            </Tooltip>
+            <Button type="primary">{buttonStr}</Button>
+        </Space>
+    </Card>
 }
 
 export default function ServicesData(services) {
@@ -38,9 +53,14 @@ export default function ServicesData(services) {
         for (let i = 0; i < services2.length; i++) {
             let name = services2[i].Name;
             let startTime = services2[i].StartTime;
+            // 计算已经过去的时间
             let status = services2[i].Status;
-            servicesList.push(service({name, startTime, status}));
+            servicesList.push(ServiceInfo({name, startTime, status}));
         }
+        servicesList.push(ServiceInfo({name: '测试1', startTime: Date.now() - 10000, status: 'start'}));
+        servicesList.push(ServiceInfo({name: '测试2', startTime: Date.now() - 20000, status: 'start'}));
+        servicesList.push(ServiceInfo({name: '测试3', startTime: Date.now() - 30000, status: 'start'}));
+        servicesList.push(ServiceInfo({name: '测试14', startTime: Date.now() - 40000, status: 'start'}));
     } else {
         return <List
             size="big"
@@ -57,19 +77,5 @@ export default function ServicesData(services) {
                 </List.Item>}
         />
     }
-    return <List
-        size="big"
-        bordered
-        dataSource={servicesList}
-        renderItem={(item) =>
-            <List.Item
-                // 居中
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                }}
-            >
-                {item}
-            </List.Item>}
-    />
+    return <Flex wrap={"wrap"} gap="large">{servicesList}</Flex>;
 }
---
+++ frontend/src/common/misc.jsx
@@ -0,0 +1,67 @@
+import {useEffect, useState} from "react";
+
+export function TimeFromStart({startTime, width}) {
+    let passTime = new Date().getTime() - new Date(startTime).getTime();
+    const [passTimeReal, setPassTimeReal] = useState(passTime);
+    const [passTimeStr, setPassTimeStr] = useState('');
+    useEffect(() => {
+        // 转换为xx天xx小时xx分xx秒
+        let day = Math.floor(passTimeReal / (24 * 3600 * 1000));
+        let leave1 = passTimeReal % (24 * 3600 * 1000);
+        let hours = Math.floor(leave1 / (3600 * 1000));
+        let leave2 = leave1 % (3600 * 1000);
+        let minutes = Math.floor(leave2 / (60 * 1000));
+        let leave3 = leave2 % (60 * 1000);
+        let seconds = Math.floor(leave3 / 1000);
+        let str = '';
+        // 数字默认两位
+        if (day > 0) {
+            str += `${day} 天 `;
+        }
+        if (hours > 0 || str !== '') {
+            str += `${hours} 小时 `;
+        }
+        if (minutes > 0 || str !== '') {
+            str += `${minutes} 分 `;
+        }
+        if (seconds > 0 || str !== '') {
+            str += `${seconds} 秒`;
+        }
+
+        setPassTimeStr(str);
+        const interval = setInterval(() => {
+            console.log("passTimeReal:", passTimeReal);
+            setPassTimeReal(passTimeReal + 1000);
+            // 转换为xx天xx小时xx分xx秒
+            let day = Math.floor(passTimeReal / (24 * 3600 * 1000));
+            let leave1 = passTimeReal % (24 * 3600 * 1000);
+            let hours = Math.floor(leave1 / (3600 * 1000));
+            let leave2 = leave1 % (3600 * 1000);
+            let minutes = Math.floor(leave2 / (60 * 1000));
+            let leave3 = leave2 % (60 * 1000);
+            let seconds = Math.floor(leave3 / 1000);
+            let str = '';
+            // 数字默认两位
+            if (day > 0) {
+                str += `${day} 天 `;
+            }
+            if (hours > 0 || str !== '') {
+                str += `${hours} 小时 `;
+            }
+            if (minutes > 0 || str !== '') {
+                str += `${minutes} 分 `;
+            }
+            if (seconds > 0 || str !== '') {
+                str += `${seconds} 秒`;
+            }
+            console.log(str);
+            setPassTimeStr(str);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [passTimeReal, passTimeStr])
+    return <div
+        style={{
+            width: width,
+        }}
+    >{passTimeStr}</div>;
+}
