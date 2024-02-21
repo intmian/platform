d16aaebe388469f8702d947badb79d7cd76f1214
mian <38103855+intmian@users.noreply.github.com>
2024-02-21 12:04:53 +0800

    基础搜索功能，有bug

--- frontend/src/admin/Config.jsx
+++ frontend/src/admin/Config.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {Button, Checkbox, Input, Space, Table} from "antd";
 import {sendGetStorage} from "../common/sendhttp.js";
 
@@ -7,20 +7,30 @@ const {Search} = Input;
 function Header({OnDataChange}) {
     const useRe = useRef(false);
     const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        sendGetStorage("", false, (data) => {
+            OnDataChange(data);
+        })
+    }, []);
+    // TODO:loading 没有数据 返回0、1正则时，严格正则，严格搜索 模糊搜索
     return <Space>
-        <Search placeholder="搜索内容"
-                onSearch={
-                    (value) => {
-                        setLoading(true);
-                        console.log(value);
-                        sendGetStorage(value, useRe.current, (data) => {
-                            OnDataChange(data);
-                            setLoading(false);
-                        })
-                    }
-                }
-                style={{width: 200}}
-                loading={loading}
+        <Input placeholder="搜索内容"
+               onChange={
+                   (value) => {
+                       if (loading) {
+                           return;
+                       }
+                       let content = value.target.value;
+                       setLoading(true);
+                       console.log(content);
+                       sendGetStorage(content, useRe.current, (data) => {
+                           OnDataChange(data);
+                           setLoading(false);
+                       })
+                   }
+               }
+               style={{width: 200}}
+               loading={loading}
         />
         <Checkbox
             onChange={(choose) => {
@@ -36,8 +46,6 @@ function Header({OnDataChange}) {
 }
 
 function Body({data}) {
-    data = data.result;
-    console.log(data);
     const columns = [
         {
             title: '键',
@@ -72,13 +80,46 @@ function Body({data}) {
         </Button>
     </Space>
     let data2 = []
-    for (let key in data) {
-        data2.push({
-            key: key,
-            type: data[key].Type,
-            value: data[key].Data,
-            operation: OprArea
-        })
+    if (data !== null) {
+        data = data.result;
+        for (let key in data) {
+            let typeStr = ''
+            switch (data[key].Type) {
+                case 1:
+                    typeStr = '字符串'
+                    break;
+                case 2:
+                    typeStr = '整数'
+                    break;
+                case 3:
+                    typeStr = '浮点'
+                    break;
+                case 4:
+                    typeStr = '布尔'
+                    break;
+                case 101:
+                    typeStr = '字符串数组'
+                    break;
+                case 102:
+                    typeStr = '整数数组'
+                    break;
+                case 103:
+                    typeStr = '浮点数组'
+                    break;
+                case 104:
+                    typeStr = '布尔数组'
+                    break;
+                default:
+                    typeStr = '未知'
+                    break;
+            }
+            data2.push({
+                key: key,
+                type: typeStr,
+                value: data[key].Data,
+                operation: OprArea
+            })
+        }
     }
     return <Table dataSource={data2} columns={columns}/>;
 }
