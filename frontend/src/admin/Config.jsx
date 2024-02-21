0a3a5302b93b023f1e4078f4073569cf6aaf2547
mian <38103855+intmian@users.noreply.github.com>
2024-02-21 17:52:36 +0800

    上传提交相关

--- frontend/src/admin/Config.jsx
+++ frontend/src/admin/Config.jsx
@@ -1,8 +1,59 @@
 import {useEffect, useRef, useState} from "react";
-import {Button, Checkbox, Input, Space, Spin, Table} from "antd";
-import {sendGetStorage} from "../common/sendhttp.js";
+import {Button, Checkbox, Input, message, Modal, Select, Space, Spin, Table,} from "antd";
+import {sendGetStorage, sendSetStorage} from "../common/sendhttp.js";
 
 const {Search} = Input;
+
+function ChangeModal({showini, onFinish, isAdd, originData}) {
+    const [show, setShow] = useState(showini);
+    if (!show) {
+        return null;
+    }
+    const canChangeKey = isAdd;
+    const canChangeType = isAdd;
+    let key = <Input
+        disabled={!canChangeKey}
+        defaultValue={originData !== null ? originData.key : ""}
+    />
+    const types = ['字符串', '整数', '浮点', '布尔', '字符串数组', '整数数组', '浮点数组', '布尔数组'];
+    let types2 = types.map((item) => {
+        return {value: item, label: item}
+    })
+    let type = <Select options={types2} disabled={!canChangeType}
+                       defaultValue={originData !== null ? originData.type : types[0]}/>
+    let value = <Input
+        disabled={!isAdd}
+        defaultValue={originData !== null ? originData.value : ""}
+        placeholder="值"
+    >
+    </Input>
+    let button = <Button
+        onClick={() => {
+            sendSetStorage(key, type.value, value.value, (data) => {
+                if (data === null || data.code !== 0) {
+                    message.error("操作失败");
+                } else {
+                    message.success("操作成功");
+                    // 通知下上层不要渲染这个节点了
+                    onFinish();
+                }
+                setShow(false);
+            })
+        }
+        }
+    >
+        {isAdd ? "新增" : "修改"}
+    </Button>
+
+    return <Modal>
+        <Space>
+            {key}
+            {type}
+            {value}
+            {button}
+        </Space>
+    </Modal>
+}
 
 function Header({OnDataChange}) {
     const useRe = useRef(false);
@@ -12,7 +63,7 @@ function Header({OnDataChange}) {
         sendGetStorage("", false, (data) => {
             OnDataChange(data);
         })
-    }, [needRefresh]);
+    }, [needRefresh, OnDataChange]);
     // TODO:loading 没有数据 返回0、1正则时，严格正则，严格搜索 模糊搜索
     return <Space>
         <Input placeholder="搜索内容"
