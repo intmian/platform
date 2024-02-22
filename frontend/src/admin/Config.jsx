8fb6d65b277c98b973c9fa07527da0b712010309
mian <38103855+intmian@users.noreply.github.com>
2024-02-22 17:53:10 +0800

    引入定义并拆分一部分组件

--- frontend/src/admin/Config.jsx
+++ frontend/src/admin/Config.jsx
@@ -1,8 +1,25 @@
 import {useEffect, useRef, useState} from "react";
 import {Button, Checkbox, Input, message, Modal, Select, Space, Spin, Table,} from "antd";
 import {sendGetStorage, sendSetStorage} from "../common/sendhttp.js";
+import {IsSliceType, ValueTypeStr} from "../common/def.js";
 
 const {Search} = Input;
+
+function ValueInput({iniValue, disabled, type, onChange}) {
+    const onRecv = (value) => {
+        if (value === null || value === undefined || value === "") {
+            return;
+        }
+        onChange(value);
+    }
+    if (!IsSliceType(type)) {
+        return <Input
+            disabled={disabled}
+            defaultValue={iniValue}
+            onChange={onRecv}
+        />
+    }
+}
 
 function ChangeModal({showini, onFinish, isAdd, originData}) {
     const [show, setShow] = useState(showini);
@@ -11,16 +28,31 @@ function ChangeModal({showini, onFinish, isAdd, originData}) {
     }
     const canChangeKey = isAdd;
     const canChangeType = isAdd;
+
+    let keyNow = useRef(null);
+    let typeNow = useRef(null);
+    const [isList, setIsList] = useState(false);
+    let valueNow = useRef(null);
+    if (originData !== null && !isAdd) {
+        keyNow.current = originData.key;
+        typeNow.current = originData.type;
+        valueNow.current = originData.value;
+    }
+
     let key = <Input
         disabled={!canChangeKey}
-        defaultValue={originData !== null ? originData.key : ""}
+        defaultValue={keyNow.current}
     />
-    const types = ['字符串', '整数', '浮点', '布尔', '字符串数组', '整数数组', '浮点数组', '布尔数组'];
-    let types2 = types.map((item) => {
-        return {value: item, label: item}
-    })
+    // let types2 = types.map((item) => {
+    //     return {value: item, label: item}
+    // })
+    let types2 = [];
+    for (let key in ValueTypeStr) {
+        types2.push({value: key, label: ValueTypeStr[key]});
+    }
     let type = <Select options={types2} disabled={!canChangeType}
                        defaultValue={originData !== null ? originData.type : types[0]}/>
+
     let value = <Input
         disabled={!isAdd}
         defaultValue={originData !== null ? originData.value : ""}
@@ -64,7 +96,7 @@ function Header({OnDataChange}) {
             OnDataChange(data);
         })
     }, [needRefresh, OnDataChange]);
-    // TODO:loading 没有数据 返回0、1正则时，严格正则，严格搜索 模糊搜索
+    // TODO:loading 没有数据 返回0、1正则时，严格正则，严格搜索 模糊搜索 动态增减表单项
     return <Space>
         <Input placeholder="搜索内容"
                onChange={
@@ -143,36 +175,7 @@ function Body({data}) {
     if (data !== null) {
         data = data.result;
         for (let key in data) {
-            let typeStr = ''
-            switch (data[key].Type) {
-                case 1:
-                    typeStr = '字符串'
-                    break;
-                case 2:
-                    typeStr = '整数'
-                    break;
-                case 3:
-                    typeStr = '浮点'
-                    break;
-                case 4:
-                    typeStr = '布尔'
-                    break;
-                case 101:
-                    typeStr = '字符串数组'
-                    break;
-                case 102:
-                    typeStr = '整数数组'
-                    break;
-                case 103:
-                    typeStr = '浮点数组'
-                    break;
-                case 104:
-                    typeStr = '布尔数组'
-                    break;
-                default:
-                    typeStr = '未知'
-                    break;
-            }
+            let typeStr = ValueTypeStr[data[key].Type];
             data2.push({
                 key: key,
                 datakey: data[key].Key,
---
+++ frontend/src/common/def.js
@@ -0,0 +1,53 @@
+/*
+* const (
+	ValueTypeNormalBegin ValueType = iota
+	ValueTypeString
+	ValueTypeInt
+	ValueTypeFloat
+	ValueTypeBool
+	ValueTypeNormalEnd
+	ValueTypeSliceBegin  = 100
+	ValueTypeSliceString = iota + ValueTypeSliceBegin - ValueTypeNormalEnd - 1
+	ValueTypeSliceInt
+	ValueTypeSliceFloat
+	ValueTypeSliceBool
+)
+* */
+
+export const ValueType = {
+    String: 1,
+    Int: 2,
+    Float: 3,
+    Bool: 4,
+    SliceBegin: 100,
+    SliceString: 101,
+    SliceInt: 102,
+    SliceFloat: 103,
+    SliceBool: 104,
+}
+
+export function IsSliceType(type) {
+    return type >= ValueType.SliceBegin;
+}
+
+export const TypeFromValueType = {
+    1: ValueType.String,
+    2: ValueType.Int,
+    3: ValueType.Float,
+    4: ValueType.Bool,
+    101: ValueType.SliceString,
+    102: ValueType.SliceInt,
+    103: ValueType.SliceFloat,
+    104: ValueType.SliceBool,
+}
+
+export const ValueTypeStr = {
+    1: '字符串',
+    2: '整数',
+    3: '浮点',
+    4: '布尔',
+    101: '字符串数组',
+    102: '整数数组',
+    103: '浮点数组',
+    104: '布尔数组',
+}
