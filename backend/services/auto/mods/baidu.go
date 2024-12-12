9d7c3ffef79824d8d946f6b50387e61738382df8
mian <38103855+intmian@users.noreply.github.com>
2024-12-12 09:23:54 +0800

    增加个废弃功能

--- backend/services/auto/mods/baidu.go
+++ backend/services/auto/mods/baidu.go
@@ -44,6 +44,10 @@ func (b *Baidu) Do() {
 	allRetry := 0
 	errs := make([]error, 0)
 	for _, v := range keys {
+		// 如果以#废弃结尾就跳过
+		if len(v) > 3 && v[len(v)-3:] == "#废弃" {
+			continue
+		}
 		unit, _ := setting.GSetting.Get(xstorage.Join("auto", "baidu", "key", "last", v))
 		var lastLink []string
 		if unit != nil {
