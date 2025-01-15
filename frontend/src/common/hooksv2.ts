6c370f004084d928bed25bb1e90484447c61cf77
mian <38103855+intmian@users.noreply.github.com>
2025-01-15 15:44:29 +0800

    fix admin: 提供移动端优化 1. 桌面端因为屏幕较大，所以要求更加严格一些

--- frontend/src/common/hooksv2.ts
+++ frontend/src/common/hooksv2.ts
@@ -2,10 +2,16 @@ import {useEffect, useState} from "react";
 
 export function useIsMobile(): boolean {
     const [isMobile, setIsMobile] = useState(false);
+    const ua = navigator.userAgent.toLowerCase();
+    const agents = ['iphone', 'ipad', 'ipod', 'android', 'linux', 'windows phone']; // 所有可能是移动端设备的字段
 
     useEffect(() => {
         const handleResize = () => {
-            setIsMobile(window.innerWidth < 768);
+            // 如果是移动端设备，或者是小屏幕，就认为是移动端
+            const isMobileWidth = window.innerWidth < 768;
+            const isMobileAgent = agents.some(agent => ua.includes(agent));
+            const isSmallScreen = window.innerWidth < 500;
+            setIsMobile((isMobileWidth && isMobileAgent) || isSmallScreen);
         };
 
         window.addEventListener("resize", handleResize);
