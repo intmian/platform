3c7133d910132f6cd0e56cec6b28870ae1f1a0ee
mian <38103855+intmian@users.noreply.github.com>
2025-05-09 18:40:03 +0800

    feat todone: 弱网环境下启动检查db

--- backend/services/todone/db/connect_mgr.go
+++ backend/services/todone/db/connect_mgr.go
@@ -58,6 +58,14 @@ func InitGMgr(setting Setting) error {
 	if err1 != nil || err2 != nil || err3 != nil || err4 != nil || err5 != nil {
 		return errors.Join(err1, err2, err3, err4, err5)
 	}
+	connectDir := GTodoneDBMgr.GetConnect(ConnectTypeDir)
+	connectGroup := GTodoneDBMgr.GetConnect(ConnectTypeGroup)
+	connectTask := GTodoneDBMgr.GetConnect(ConnectTypeTask)
+	connectTags := GTodoneDBMgr.GetConnect(ConnectTypeTags)
+	connectSubGroup := GTodoneDBMgr.GetConnect(ConnectTypeSubGroup)
+	if connectDir == nil || connectGroup == nil || connectTask == nil || connectTags == nil || connectSubGroup == nil {
+		return errors.New("connect is nil")
+	}
 	return nil
 }
 
