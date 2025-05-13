9714dc56fde6c89109ae99ed3e6e4571ca281703
mian <38103855+intmian@users.noreply.github.com>
2025-05-13 14:03:35 +0800

    feat global: 添加ConfigProvider以支持国际化配置

--- frontend/src/common/globalCtx.jsx
+++ frontend/src/common/globalCtx.jsx
@@ -1,9 +1,14 @@
 import {LoginProvider} from "./loginCtx.jsx";
+import 'moment/dist/locale/zh-cn';
+import zhCN from 'antd/lib/locale/zh_CN';
+import {ConfigProvider} from "antd";
 
 export function GlobalCtx({children}) {
     return (
-        <LoginProvider>
-            {children}
-        </LoginProvider>
+        <ConfigProvider locale={zhCN}>
+            <LoginProvider>
+                {children}
+            </LoginProvider>
+        </ConfigProvider>
     );
 }
