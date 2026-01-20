8a0cebfe0b161a1a3fc5e4ee18c0ddcbf983da20
fangjiayan <1437131326@qq.com>
2026-01-20 19:56:53 +0800

    fix todone: unfinish为空时新建的会不显示
    修复1 紧急修复

--- backend/services/todone/logic/sub_group.go
+++ backend/services/todone/logic/sub_group.go
@@ -337,7 +337,7 @@ func (s *SubGroupLogic) CreateTask(userID string, title, note string, taskType d
 	task.BindOutTags(make([]string, 0))
 
 	// 更新缓存和序列
-	if s.unFinTasksCache != nil && len(s.unFinTasksCache) > 0 {
+	if s.unFinTasksLoaded {
 		s.unFinTasksCache[task.dbData.TaskID] = task
 		task.BindOutIndex(s.taskSequence.GetSequenceOrAdd(parentTaskID, task.dbData.TaskID))
 		err = s.OnChangeSeq()
